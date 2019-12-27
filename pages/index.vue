3f082d58b8086506bc105bd872c2f4007a26eea9
uni51 <uni51@users.noreply.github.com>
2019-12-27 15:32:31 +0900

    Section06：非同期通信

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- {{ users[0].id }}, {{ users[0].name }} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }}, {{ user.name }}, {{ user.company.name }}
+        </li>
+      </ul>  
+    </div>
+  </section>
+</template>
+
+<script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/usersxxx'
+
+export default {
+  asyncData({ params, error }) {
+    return axios.get(url)
+      .then( (response) => {
+        return { users: response.data }
+      })
+      .catch((e => {
+        // console.log(e.response.status)
+        // error({ users: e.response.status, message: e.message })
+        error({ users: e.response.status, message: 'Error!!!!' })
+      }))
+  }
+}
+</script>
+
+<style>
+</style>
